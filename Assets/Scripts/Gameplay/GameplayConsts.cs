66a807d928749b2ae98d84d37533b947d9c054d3
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-02-14 13:55:41 +0300

    [fix] gameplay consts

---
+++ Assets/Scripts/Gameplay/GameplayConsts.cs
@@ -0,0 +1,13 @@
+namespace Gameplay
+{
+    public static class GameplayConsts
+    {
+        public const float SlimeMinScale = 1;
+        public const float SlimeMaxScale = 2;
+        public const float SlimeMaxSweetnessForDestroy = 60;
+        public const float SlimeFeaturesDivider = 100;
+        public const float SlimeFeaturesDefaultValue = 10;
+        public const float SlimeMinAnimationSpeed = 0.1f;
+        public const float SlimeMaxAnimationSpeed = 1;
+    }
+}
